c0e6860b566a544de4fc404212f53f5f651c53b4
MisterKilgore <mcantoni81@gmail.com>
2022-12-02 22:53:55 +0100

    Repo init

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ladybird',
+    version='0.0.1',
+    author='Michele Cantoni',
+    author_email='mcantoni81@gmail.com',
+    description='Various tools for social media analysis',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/misterkilgore/ladybird',
+    project_urls = {
+        "Bug Tracker": "https://github.com/misterkilgore/ladybird/issues"
+    },
+    license='MIT',
+    packages=['ladybird'],
+    install_requires=[],
+)
